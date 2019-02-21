1b31f23e8878f722a3311bb3c07f735742e857a3
bdowds <bwdowds@gmail.com>
2019-02-21 17:35:58 -0600

    Added instructions for project

--- OpenHouse/Program.cs
+++ OpenHouse/Program.cs
@@ -6,7 +6,22 @@ namespace OpenHouse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Open House!");
+
+            // Your goal is to lead a user, through the use of WriteLine's and ReadLine's,
+            // through an open house. This is an open ended project that is meant to be
+            // creative. 
+            // A good place to start is by introducing the open house and asking the
+            // user if they would like to see a particular room. 
+            // Ex. "Welcome to the open house", "Would you like to see the kitchen?"
+            //      then grab the users response and continue your program based on their answer.
+
+            // Some things to keep in mind:
+            //  -Use methods when repeating segments of code.
+            //    ex. If you are testing for "yes" or "no" as a user response for multiple questions
+            //        try making a method that takes a question and checks if the user entered "yes" or "no".
+            //  -Make commits in git as you go, and work on a branch.
+            //  -This is a self driven project, the goal is to push yourself and see what you can build.
         }
     }
 }
