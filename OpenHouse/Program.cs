867347122343905f46cd387def5d72f9f8a995da
Rjones <rjonessoft@gmail.com>
2019-02-21 19:57:12 -0600

    Built GetResponse method and implemented living room and kitchen

--- OpenHouse/Program.cs
+++ OpenHouse/Program.cs
@@ -4,24 +4,54 @@ namespace OpenHouse
 {
     class Program
     {
+        static bool GetResponse()
+        {
+            while (true)
+            {
+                string response = Console.ReadLine();
+
+                if (response  == "yes")
+                {
+                    return true;
+                }
+                else if (response == "no")
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("Please type yes or no.");
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Open House!");
 
-            // Your goal is to lead a user, through the use of WriteLine's and ReadLine's,
-            // through an open house. This is an open ended project that is meant to be
-            // creative. 
-            // A good place to start is by introducing the open house and asking the
-            // user if they would like to see a particular room. 
-            // Ex. "Welcome to the open house", "Would you like to see the kitchen?"
-            //      then grab the users response and continue your program based on their answer.
+            string[] rooms = new string[]{ "living room", "kitchen" };
+            string[] descriptions = new string[] { "The living room has shag carpet.", "The kitchen has black and white tile floors." };
+            
+            for(int i = 0; i < rooms.Length; i++)
+            {
+                Console.WriteLine($"Would you like to see the {rooms[i] }?");
 
-            // Some things to keep in mind:
-            //  -Use methods when repeating segments of code.
-            //    ex. If you are testing for "yes" or "no" as a user response for multiple questions
-            //        try making a method that takes a question and checks if the user entered "yes" or "no".
-            //  -Make commits in git as you go, and work on a branch.
-            //  -This is a self driven project, the goal is to push yourself and see what you can build.
+                if (GetResponse() == true)
+                {
+                    Console.WriteLine(descriptions[i]);
+                }
+            }
         }
-    }
-}
+
+
+
+           
+           
+                
+
+         
+
+            
+          
+          
+    
